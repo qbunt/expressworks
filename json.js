0e0b8137ab3b1adf9753176b3a9c60b8d993fbeb
Jeremy Bunting <buntingj@gmail.com>
2017-05-13 14:16:48 -0700

    current progress

--- json.js
+++ json.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
-var fs = require('fs')
-var obj;
+var fs = require('fs');
+
 app.get('/books', (req, res)=>{
     console.log(process.argv[3])
     fs.readFile(process.argv[3], (err, data)=>{
